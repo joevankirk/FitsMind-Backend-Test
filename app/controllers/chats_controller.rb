e85c4a7b6bd26778a3691c7fb99d5f283a4678b4
Joe Van Kirk <imjoevankirk@gmail.com>
2017-09-08 14:12:28 -0500

    added logic to chats controller

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,34 @@
+class ChatsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+  before_action :set_user, only: [ :new, :create, :index ]
+
+  def index
+    user1_chats = Chat.where(user_1_id: @user.id)
+    user2_chats = Chat.where(user_2_id: @user.id)
+
+    @chats = user1_chats + user2_chats
+  end
+
+  def new
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = Chat.new(chat_params)
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:user_1_id, :user_2_id)
+  end
+
+  def set_chat
+    @chat = Chat.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(current_user[:id])
+  end
+
+end
