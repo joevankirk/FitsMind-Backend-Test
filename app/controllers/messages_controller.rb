9937c5063b120c355314d94d70b9e0ba20578fe3
Joe Van Kirk <imjoevankirk@gmail.com>
2017-09-08 14:34:06 -0500

    added logic to messages controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,36 @@
+class MessagesController < ApplicationController
+  before_action :set_chat, only:[:index, :create]
+  before_action :authenticate_user!
+
+  def index
+    # show all messages
+    @messages = @chat.messages.all
+    @messages = @messages.sort_by(&:created_at)
+    @messages = @messages.last(10)
+
+    if current_user.id == @chat.user_1_id
+      recipient_id = @chat.user_2_id
+      @recipient = User.find(recipient_id)
+    else
+      recipient_id = @chat.user_1_id
+      @recipient = User.find(recipient_id)
+    end
+    @message = current_user.messages.build
+  end
+
+  def create
+    @message = current_user.messages.build(message_params)
+    @message.save
+  end
+
+  private
+
+  def set_chat
+    @chat = Chat.find(params[:chat_id])
+  end
+
+
+  def message_params
+    params.require(:message).permit(:content, :sender_id, :recipient_id, :chat_id)
+  end
+end
