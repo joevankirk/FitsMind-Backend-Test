a261afae75b4d9e4d30cebb5060ef612327fad30
Joe Van Kirk <imjoevankirk@gmail.com>
2017-09-08 13:07:45 -0500

    added Message model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908175852) do
+ActiveRecord::Schema.define(version: 20170908180701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20170908175852) do
     t.integer  "user_2_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "chat_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "content"
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.index ["chat_id"], name: "index_messages_on_chat_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +51,5 @@ ActiveRecord::Schema.define(version: 20170908175852) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "messages", "chats"
 end
